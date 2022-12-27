9690c23cfb2e0d7e08a17eb655340388ea80fd8f
esna <eunning09005@gmail.com>
2022-12-27 20:45:46 +0900

    프로그램 초기 세팅

--- NaEunSu/Sokoban/Sokoban/Program.cs
+++ NaEunSu/Sokoban/Sokoban/Program.cs
@@ -6,6 +6,64 @@ Console.BackgroundColor = ConsoleColor.DarkYellow; // 배경색을 설정한다
 Console.ForegroundColor = ConsoleColor.White; // 글꼴색을 설정한다
 Console.Clear(); // 출력된 모든 내용을 지운다
 
+// 게임 루프
+// 플레이어 좌표 설정
+int playerX = 0;
+int playerY = 0;
+
+int boxX = 5;
+int boxY = 5;
+
+// 게임 루프 == 프레임
+// 가로 15 세로 10
+while (true)
+{
+    Console.Clear(); // 이전 프레임을 지운다
+    // ------------------------------ Render -------------------------
+    // 플레이어 출력하기
+
+
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write("N");
+
+    Console.SetCursorPosition(boxX, boxY);
+    Console.Write("■");
+
+
+    // ------------------------------ ProcessInput ----------------------
+
+    ConsoleKey key = Console.ReadKey().Key;
+
+    // --------------------------- Update ------------------------------
+    // 오른쪽 화살표키를 눌렀을 때
+    if (key == ConsoleKey.RightArrow)
+    {
+        playerX = Math.Min(playerX + 1, 15); // 오른쪽으로 이동 
+        boxX = Math.Min(boxX , 14);
+    }
+
+    if (key == ConsoleKey.LeftArrow)
+    {
+        playerX = Math.Max(0, playerX - 1); // 왼쪽으로 이동
+        boxX = Math.Max(0, boxX - 2);
+    }
+
+    if (key == ConsoleKey.UpArrow)
+    {
+        playerY = Math.Max(0, playerY - 1); ; // 위쪽으로 이동
+        boxY = Math.Max(0, boxY - 2);
+    }
+
+    if (key == ConsoleKey.DownArrow)
+    {
+        playerY = Math.Min(playerY + 1, 10); // 아래쪽으로 이동
+        boxY = Math.Min(boxY, 9);
+    }
+    
+
+
+
+}
 
 
 
